8dd72ee2c09b92b867e446da2ac93f0c8c66eb22
josuealban <mjalban@sudamericano.edu.ec>
2025-01-09 10:12:15 -0500

    Update index.js

--- ejerciciosmom3/ejercicio-3m-04/index.js
+++ ejerciciosmom3/ejercicio-3m-04/index.js
@@ -6,14 +6,7 @@ const combinaciones = [];
 
 // Generar combinaciones de dos dígitos sin repetir números y sin invertir
 for (let i = 0; i < numeros.length; i++) {
-    for (let j = 0; j < numeros.length; j++) {
-        // Asegurarse de que los números no se repitan y que no se inviertan
-        if (numeros[i] !== numeros[j] && i < j) {
-            // Formar la combinación y agregarla al array
-            combinaciones.push(`${numeros[i]}${numeros[j]}`);
-        }
-    }
+    for (let j = i+1 ;j < numeros.length; j++) {
+        alert( numeros[i]+""+numeros[j])
+       }
 }
-
-// Mostrar las combinaciones en un alert
-alert(combinaciones.join(", "));
